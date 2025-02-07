d89af9995ae905b95a31c972f4d63f814d4b13dc
342763802 <jayden.shi@student.tdsb.on.ca>
2025-02-07 17:48:58 -0500

    updated lesson1 with new code

--- Homework/lesson1/src/App.java
+++ Homework/lesson1/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("\nInput a decimal: ");
+
+        int input = (int) keyboard.nextDouble();
+
+        keyboard.close();
+
+        System.out.println("Your input as an integer is: " + input);
+
     }
+
 }
